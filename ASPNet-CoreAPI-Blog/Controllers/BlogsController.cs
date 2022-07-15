70876aed135cd56f446cf9c8ad6fd0b9418bc4f9
KhangVuVan21a <khang21a@gmail.com>
2022-07-15 16:01:39 +0700

    API done not have base response

--- ASPNet-CoreAPI-Blog/Controllers/BlogsController.cs
+++ ASPNet-CoreAPI-Blog/Controllers/BlogsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ASPNet_CoreAPI_Blog.Models;
+using ASPNet_CoreAPI_Blog.Models.DTO;
 
 namespace ASPNet_CoreAPI_Blog.Controllers
 {
@@ -14,6 +15,7 @@ namespace ASPNet_CoreAPI_Blog.Controllers
     public class BlogsController : ControllerBase
     {
         private readonly BlogContext _context;
+        private List<string> listposition = new List<string>() { "Viet Nam", "Chau A", "Chau Au", "Chau My"};
 
         public BlogsController(BlogContext context)
         {
@@ -22,51 +24,97 @@ namespace ASPNet_CoreAPI_Blog.Controllers
 
         // GET: api/Blogs
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Blog>>> GetBlogs()
+        public async Task<ActionResult<IEnumerable<Object>>> GetBlogs()
         {
           if (_context.Blogs == null)
           {
               return NotFound();
           }
-            return await _context.Blogs.ToListAsync();
+            /*return await _context.Blogs.ToListAsync();*/
+            var blogs =  from blog in _context.Blogs select new{
+                blog.Id,
+                blog.title,
+                blog.description,
+                blog.detail,
+                blog.thumb,
+                blog.datePublic,
+                blog.status,
+                blog.category,
+                blog.Positions
+            };
+            return blogs.ToList();
         }
 
         // GET: api/Blogs/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Blog>> GetBlog(int id)
+        public async Task<ActionResult<Object>> GetBlog(int id)
         {
           if (_context.Blogs == null)
           {
               return NotFound();
           }
-            var blog = await _context.Blogs.Where(x => x.Id == id)
-                         .Include(x => x.category)
-                         .FirstOrDefaultAsync();
-            var c = blog.category;
-
-            if (blog == null)
+            /*var blog = await _context.Blogs.Where(x => x.Id == id)
+                         .Include(x => x.category).Include(x=>x.Positions)
+                         .FirstOrDefaultAsync();*/
+            var test = from blog in _context.Blogs where blog.Id == id select new
+            {
+                blog.Id,
+                blog.title,
+                blog.description,
+                blog.detail,
+                blog.thumb,
+                blog.datePublic,
+                blog.status,
+                blog.CategoryId,
+                blog.category,
+                blog.Positions
+            } ;
+            if (test == null)
             {
                 return NotFound();
             }
 
-            return blog;
+            return Accepted(test.First());
         }
 
         // PUT: api/Blogs/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutBlog(int id, Blog blog)
+        public async Task<IActionResult> PutBlog(int id, BlogDTO blogDTO,string listpos)
         {
-            if (id != blog.Id)
+            List<int> list = listpos.Split(',').Select(Int32.Parse).ToList();
+            if (id != blogDTO.Id)
             {
                 return BadRequest();
             }
-
+            Blog blog = new Blog()
+            {
+                Id = blogDTO.Id,
+                title = blogDTO.title,
+                description = blogDTO.description,
+                detail = blogDTO.detail,
+                thumb = blogDTO.thumb,
+                datePublic = blogDTO.datePublic,
+                status = blogDTO.status,
+                CategoryId = blogDTO.CategoryId,
+                category = _context.Categories.Find(blogDTO.CategoryId),
+                Positions = null,
+            };
             _context.Entry(blog).State = EntityState.Modified;
-
+            _context.Positions.RemoveRange(_context.Positions.Where(x => x.BlogId == id).ToList());
+            foreach (var item in list)
+            {
+                Position position = new Position()
+                {
+                    Id = 0,
+                    Name = listposition[item - 1].ToString(),
+                    BlogId = blog.Id
+                };
+                _context.Positions.Add(position);
+            }
             try
             {
-                await _context.SaveChangesAsync();
+               _context.SaveChanges();
             }
             catch (DbUpdateConcurrencyException)
             {
@@ -86,14 +134,40 @@ namespace ASPNet_CoreAPI_Blog.Controllers
         // POST: api/Blogs
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Blog>> PostBlog(Blog blog)
+        public async Task<ActionResult<Blog>> PostBlog(BlogDTO blogDTO, string listpos)
         {
+           List<int> list = listpos.Split(',').Select(Int32.Parse).ToList();
           if (_context.Blogs == null)
           {
               return Problem("Entity set 'BlogContext.Blogs'  is null.");
           }
+            Blog blog = new Blog()
+            {
+                Id= 0,
+                title=blogDTO.title,
+                description=blogDTO.description,
+                detail=blogDTO.detail,
+                thumb=blogDTO.thumb,
+                datePublic=blogDTO.datePublic,
+                status=blogDTO.status,
+                CategoryId=blogDTO.CategoryId,
+                category=_context.Categories.Find(blogDTO.CategoryId),
+                Positions=null,
+            };
             _context.Blogs.Add(blog);
-            await _context.SaveChangesAsync();
+            _context.SaveChanges();
+            var idblog = blog.Id;
+            foreach (var id in list)
+            {
+                Position position=new Position()
+                {
+                    Id = 0,
+                    Name = listposition[id - 1].ToString(),
+                    BlogId = blog.Id
+                };
+                _context.Positions.Add(position) ;
+            }
+            _context.SaveChanges();
 
             return CreatedAtAction("GetBlog", new { id = blog.Id }, blog);
         }
@@ -111,7 +185,7 @@ namespace ASPNet_CoreAPI_Blog.Controllers
             {
                 return NotFound();
             }
-
+            _context.Positions.RemoveRange(_context.Positions.Where(x => x.BlogId == blog.Id).ToList());
             _context.Blogs.Remove(blog);
             await _context.SaveChangesAsync();
 
---
+++ ASPNet-CoreAPI-Blog/Models/DTO/BlogDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace ASPNet_CoreAPI_Blog.Models.DTO
+{
+    public class BlogDTO
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string detail { get; set; }
+        public string? thumb { get; set; }
+        public DateTime datePublic { get; set; }
+        public bool status { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
